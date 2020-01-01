06bb09aeff96683c9a90451128804d6d8755d65d
ParasAvkirkar <parasavkirkar96@gmail.com>
2019-12-31 21:08:46 -0500

    final refactor

--- q1.r
+++ q1.r
@@ -1,19 +1,11 @@
-first_set = read.csv("./dataset1.csv", quote="")
+first_set = read.csv("dataset1.csv", quote="")
 # View(first_set)
 
 ### Performing ANOVA test ###
-significance_level = 0.05
 result = aov(time ~ menu, data=first_set)
 summary(result)
 p_value = summary(result)[[1]]$Pr[[1]]
 cat("P-value for difference in variance of menu usage timings ", p_value, "\n")
-if (p_value < significance_level) {
-  print("Null hypothesis: All menus are similar, is rejected")
-  print("Some of the menus's exhibit different usage timings")
-} else {
-  print("Alternative hypothesis is rejected, all menus have similar usage timings")
-  print("Menus exhibit similar usage timings")
-}
 
 ### End ANOVA test ###
 
@@ -31,66 +23,15 @@ By now, we have just learned that some of the menus have
 all_pair_test = pairwise.t.test(first_set$time, first_set$menu, p.adjust.method = "bonferroni")
 all_pair_test
 
-### Individual pairwise test ###
-
 toolglass_users = subset(first_set, menu == "toolglass")
-flowmenu_users = subset(first_set, menu == "flowmenu")
 toolpalette_users = subset(first_set, menu == "toolpalette")
 controlmenu_users = subset(first_set, menu == "controlmenu")
+flowmenu_users = subset(first_set, menu == "flowmenu")
 
-glass_vs_flow = t.test(toolglass_users$time, flowmenu_users$time, var.equal = TRUE)
-glass_vs_flow
-
-glass_vs_palette = t.test(toolglass_users$time, toolpalette_group$time, var.equal = TRUE)
-glass_vs_palette
-
-glass_vs_control = t.test(toolglass_users$time, controlmenu_users$time, var.equal = TRUE)
-glass_vs_control
-
-flow_vs_palette = t.test(flowmenu_users$time, toolpalette_users$time, var.equal = TRUE)
-flow_vs_palette
-
-flow_vs_control = t.test(flowmenu_users$time, controlmenu_users$time, var.equal = TRUE)
-flow_vs_control
-
-palette_vs_control = t.test(toolpalette_users$time, controlmenu_users$time, var.equal = TRUE)
-palette_vs_control
-
-"
-  Our significance level is 0.05
-  The p-value lower than significance level was found only for
-  toolpalette and controlmenu (0.034).
-  For other pairs, p-values was found greater than 0.05
-  Hence, we reject the null hypothesis in case of toolpalette vs controlmenu
-  For other pairs, we consider time performance identical.
-  Still, we have to figure out which one is better between toolpalette and controlmenu.
-  
-  We will perform one-sided tail t-test between toolpalette and controlmenu samples
-  
-"
-
+### One-sided test to compare performance of toolpalette vs controlmenu
 result = t.test(toolpalette_users$time, controlmenu_users$time, alternative = "greater")
 result
-significance_level = 0.05
-if (result$p.value < significance_level) {
-  print("Reject Null Hypothesis")
-  print("Toolpalette menu needs more access time compared to controlmenu")
-  print("Controlmenu gives better performance")
-} else {
-  print("Reject alternative hypothesis")
-  print("Controlmenu gives inferior or similar performance")
-}
 
-result = t.test(toolpalette_users$time, toolglass_users$time, alternative = "greater")
-significance_level = 0.05
-if (result$p.value < significance_level) {
-  print("Reject Null Hypothesis")
-  print("Toolpalette menu needs more access time compared to toolglass")
-  print("toolglass gives better performance")
-} else {
-  print("Reject alternative hypothesis")
-  print("toolglass gives inferior or similar performance")
-}
 
 ### End of pairwise-t-test
   
--- q2.r
+++ q2.r
@@ -1,49 +1,21 @@
-second_set = read.csv("./dataset2.csv", quote="")
+second_set = read.csv("dataset2.csv", quote="")
 
 ### Begin MANOVA test ###
 result = manova(cbind(time, error) ~ menu, data=second_set)
 result_summary = summary(result, test="Pillai")
 result_summary
-"
-  Have explored all the test variations of manova: Pillai, Wilks, Hotelling-Lawley, Roy
-  For all the test methods p_value was less than 0.05
-  Only for assignment/presentation purposes have shown Pillai test method  
-"
 
 p_value = result_summary$stats[[1,6]]
-significance_level = 0.05
-if (p_value < significance_level) {
-  print("Reject the null hypothesis")
-  print("There is variation in performance between menus in terms of time and error")
-} else {
-  print("Reject the alternative hypothesis")
-  print("There is no difference between types of menus in terms of time and error")
-}
+cat("P-value observed ", p_value, "\n")
 ### End MANOVA test ###
 
-"
-  After applying MANOVA we know that time and error dependent variables are not same across menus
-  To identify which is different between time and error, we apply ANOVA again  
-"
 summary.aov(result)
 aov_result = summary.aov(result)
 time_p_value = aov_result$` Response time`$`Pr(>F)`[[1]]
 error_p_value = aov_result$` Response error`$`Pr(>F)`[[1]]
-if (time_p_value >= significance_level) {
-  print("Among time and error dependent variables, time was not significant between menu groups")
-}
-if (error_p_value >= significance_level) {
-  print("Among time and error dependent variables, error was not significant between menu groups")
-}
 
 ### Pairwise t-test ###
-"
-By now, we have just learned that some of the menus have
-  different global population mean for error value.
-  
-  To infer which menus have better or similar performance, in terms of error,
-  we have to perform pairwise t-test
-"
+### Pairwise t-test 
 
 t_test_result = pairwise.t.test(second_set$time, second_set$menu, paired=TRUE, p.adjust.method = "bonferroni")
 print(t_test_result)
@@ -51,44 +23,24 @@ print(t_test_result)
 t_test_result = pairwise.t.test(second_set$error, second_set$menu, paired=TRUE, p.adjust.method = "bonferroni")
 print(t_test_result)
 
-"
-  Our significance level is 0.05
-  The p-value lower than significance level was found for:
-  1. toolglass vs flowmenu (0.00117)
-  2. toolglass vs controlmenu (0.00018)
-  For other pairs, p-values was found greater than 0.05
-  Hence, we reject the null hypothesis in case of:
-  toolglass vs toolpalettte, toolpalette vs flowmenu, toolpalette vs controlmenu, flowmenu vs controlmenu
-  For other pairs, we consider performance in terms of error is identical.
-  
-  Still, we have to figure out which one is better between: toolglass, toolpalette, flowmenu
-  We will perform one-sided tail t-test for each above pair
-"
 toolglass_group = subset(second_set, menu == "toolglass")
 toolpalette_group = subset(second_set, menu == "toolpalette")
 controlmenu_group = subset(second_set, menu == "controlmenu")
 flowmenu_group = subset(second_set, menu == "flowmenu")
 
-one_side_t_test_result = t.test(toolglass_group$error, controlmenu_group$error, alternative = "less")
+one_side_t_test_result = t.test(toolglass_group$error, controlmenu_group$error, alternative = "less", paired = TRUE)
 one_side_t_test_result
-if (one_side_t_test_result$p.value < significance_level) {
-  print("Toolglass menu shows less error than toolpalette group")
-  print("Toolglass menu is better than toolpalette group")
-}
 
-one_side_t_test_result = t.test(toolglass_group$error, controlmenu_group$error, alternative = "less")
+one_side_t_test_result = t.test(toolglass_group$error, flowmenu_group$error, alternative = "less", paired = TRUE)
 one_side_t_test_result
-if (one_side_t_test_result$p.value < significance_level) {
-  print("Toolglass menu shows less error than controlmenu group")
-  print("Toolglass menu is better than controlmenu group")
-}
 
-one_side_t_test_result = t.test(toolglass_group$error, flowmenu_group$error, alternative = "less")
+one_side_t_test_result = t.test(toolpalette_group$time, controlmenu_group$time, alternative = "greater", paired = TRUE)
 one_side_t_test_result
-if (one_side_t_test_result$p.value < significance_level) {
-  print("Toolglass menu shows less error than flowmenu group")
-  print("Toolglass menu is better than flowmenu group")
-}
 
+one_side_t_test_result = t.test(toolpalette_group$time, flowmenu_group$time, alternative = "greater", paired = TRUE)
+one_side_t_test_result
+
+one_side_t_test_result = t.test(toolpalette_group$time, toolglass_group$time, alternative = "greater", paired = TRUE)
+one_side_t_test_result
 
 ### End of pairwise-t-test ###
